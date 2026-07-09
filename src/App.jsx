73f519e7599b8f3dfbe973d91cdb5c0b03b5cd08
Courressa <courressamalcolm2030@gmail.com>
2026-07-09 00:29:38 -0400

    Add todo list and form components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
+import TodoList from "./TodoList.jsx";
+import TodoForm from "./TodoForm.jsx";
 import './App.css'
 
 function App() {
-  const todoList = [
-    {id: 1, title: "wash the car"},
-    {id: 2, title: "fold laundry"},
-    {id: 3, title: "eat strawberry cake"}
-  ];
-
   return (
     <div>
-      <h1>ToDo List</h1>
-      <ul>
-        {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
-      </ul>
+      <h1>Todo List</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   )
 }
---
+++ src/TodoList.jsx
@@ -0,0 +1,15 @@
+const TodoList = () => {
+    const todoList = [
+        {id: 1, title: "wash the car"},
+        {id: 2, title: "fold laundry"},
+        {id: 3, title: "eat strawberry cake"}
+    ];
+
+    return (
+        <ul>
+            {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
+        </ul>
+    );
+}
+
+export default TodoList;
