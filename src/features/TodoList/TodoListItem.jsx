72ab2d1b96bc93fa1c6f0d0612ce3e606355aeab
Courressa <courressamalcolm2030@gmail.com>
2026-08-09 04:21:20 -0400

    Create custom hook for title editing

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,26 +1,22 @@
-import { useState } from "react";
 import TextInputWithLabel from "../../shared/TextInputWithLabel";
 import { isValidTodoTitle } from "../../utils/todoValidation";
+import { useEditableTitle } from "../../hooks/useEditableTitle";
 
 function TodoListItem({todo, onCompleteTodo, onUpdateTodo}) {
-  const [isEditing, setIsEditing] = useState(false);
-  const [workingTitle, setWorkingTitle] = useState(todo.title);
-
-  const handleCancel = () => {
-    setWorkingTitle(todo.title);
-    setIsEditing(false);
-  };
-
-  const handleEdit = (event) => {
-    setWorkingTitle(event.target.value);
-  };
+  const {
+    isEditing,
+    workingTitle,
+    startEditing,
+    cancelEdit,
+    updateTitle,
+    finishEdit} = useEditableTitle(todo.title);
 
   const handleUpdate = (event) => {
     if (!isEditing) return;
 
     event.preventDefault();
-    onUpdateTodo({...todo, title: workingTitle});
-    setIsEditing(false);
+    const finalTitle = finishEdit();
+    onUpdateTodo({ ...todo, title: finalTitle });
   };
 
   return (
@@ -28,8 +24,8 @@ function TodoListItem({todo, onCompleteTodo, onUpdateTodo}) {
       <form onSubmit={handleUpdate}>
         { isEditing ? (
           <>
-            <TextInputWithLabel value={workingTitle} onChange={handleEdit} />
-            <button type="button" onClick={handleCancel}>Cancel</button>
+            <TextInputWithLabel value={workingTitle} onChange={(event) => updateTitle(event.target.value)} />
+            <button type="button" onClick={cancelEdit}>Cancel</button>
             <button type="button" onClick={handleUpdate} disabled={!isValidTodoTitle(workingTitle)}>Update</button>
           </>
         ) : (
@@ -42,7 +38,7 @@ function TodoListItem({todo, onCompleteTodo, onUpdateTodo}) {
                 onChange={() => onCompleteTodo(todo.id)}
               />
             </label>
-            <span onClick={() => setIsEditing(true)}>{todo.title}</span>
+            <span onClick={() => startEditing()}>{todo.title}</span>
           </>
         )}
       </form>
