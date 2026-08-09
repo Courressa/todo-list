b95f321fd1eeb91f4e1288744ef96bd174438d82
Courressa <courressamalcolm2030@gmail.com>
2026-08-09 03:38:52 -0400

    Setup helper function for validation

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from "react";
 import TextInputWithLabel from "../shared/TextInputWithLabel";
+import { isValidTodoTitle } from "../utils/todoValidation";
+
 
 function TodoForm({ onAddTodo }) {
     const inputRef = useRef();
@@ -24,7 +26,7 @@ function TodoForm({ onAddTodo }) {
                 elementId={"todoTitle"}
                 labelText={"Todo"}
             />
-            <button type="submit" disabled={!workingTodoTitle.trim()}>Add Todo</button>
+            <button type="submit" disabled={!isValidTodoTitle(workingTodoTitle)}>Add Todo</button>
         </form>
     );
 }
